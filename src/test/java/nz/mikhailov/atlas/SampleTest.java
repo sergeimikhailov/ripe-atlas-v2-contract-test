1571a341568ff7c88e44be6889f16926eb38a2fb
Sergei Mikhailov <sergei@mikhailov.nz>
2017-03-25 09:42:04 +1300

    Good intentions

---
+++ src/test/java/nz/mikhailov/atlas/SampleTest.java
@@ -0,0 +1,15 @@
+package nz.mikhailov.atlas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+  @Test
+  public void testSomeLibraryMethod() {
+
+    assertTrue(true);
+  }
+
+}
